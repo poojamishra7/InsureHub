791d2e26811ae8f1f2210c232b3d79df7e93a8b6
poojamishra7 <82871470+poojamishra7@users.noreply.github.com>
2026-04-16 14:08:03 +0530

    Create purchasesController.js

---
+++ controllers/purchasesController.js
@@ -0,0 +1,28 @@
+class PurchasesController {
+    // Create a new purchase
+    createPurchase(req, res) {
+        // Implementation goes here
+    }
+
+    // Get purchase by ID
+    getPurchaseById(req, res) {
+        // Implementation goes here
+    }
+
+    // Get all purchases for a customer
+    getCustomerPurchases(req, res) {
+        // Implementation goes here
+    }
+
+    // Update payment status
+    updatePaymentStatus(req, res) {
+        // Implementation goes here
+    }
+
+    // Get all purchases
+    getAllPurchases(req, res) {
+        // Implementation goes here
+    }
+}
+
+module.exports = new PurchasesController();
