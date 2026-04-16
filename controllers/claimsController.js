d886fa0b1663a4798f62846caf6aa63bb3adf0ce
poojamishra7 <82871470+poojamishra7@users.noreply.github.com>
2026-04-16 14:11:30 +0530

    Create claimsController.js

---
+++ controllers/claimsController.js
@@ -0,0 +1,30 @@
+// claimsController.js
+
+class ClaimsController {
+    
+    async submitClaim(req, res) {
+        // Functionality to submit a claim
+    }
+
+    async getClaimById(req, res) {
+        // Functionality to get a claim by ID
+    }
+
+    async getCustomerClaims(req, res) {
+        // Functionality to get all claims for a customer
+    }
+
+    async updateClaimStatus(req, res) {
+        // Functionality to update claim status
+    }
+
+    async getAllClaims(req, res) {
+        // Functionality to get all claims
+    }
+
+    async getClaimStatistics(req, res) {
+        // Functionality to get claims statistics
+    }
+}
+
+module.exports = new ClaimsController();
